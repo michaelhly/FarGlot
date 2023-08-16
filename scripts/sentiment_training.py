b84ba3ff293758ffadb001869aa28d31a30ad078
michaelhly <michaelhly@gmail.com>
2023-08-16 13:23:43 -0400

    Fix sentiment_training script

--- scripts/sentiment_training.py
+++ scripts/sentiment_training.py
@@ -1,9 +1,12 @@
-import os
 import numpy as np
-from datasets import load_dataset, load_metric
+from datasets import load_dataset
+from evaluate import load
 from transformers import AutoModelForSequenceClassification, AutoTokenizer, DataCollatorWithPadding, TrainingArguments, Trainer
 
 OUT_DIR="data"
+
+score2label = { 1: 'POSITIVE', -1: 'NEGATIVE', 0: 'NEUTRAL' }
+label2score = { v: k for k, v in score2label.items() }
 
 dataset = load_dataset(
     "csv",
@@ -12,20 +15,13 @@ dataset = load_dataset(
         'train': "data/training-set.csv"
     })
 
-# set the wandb project where this run will be logged
-os.environ["WANDB_PROJECT"] = "sentiment-training"
-# save your trained model checkpoint to wandb
-os.environ["WANDB_LOG_MODEL"] = "true"
-# turn off watch to log faster
-os.environ["WANDB_WATCH"] = "false"
-# wandb base url
-os.environ["WANDB_BASE_URL"] = "http://localhost:8080"
-
 ### PREPROCESSING ###
 
 tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
-def preprocess_function(examples):
-   return tokenizer(examples["text"], truncation=True)
+def preprocess_function(batch):
+   tokenized_batch = tokenizer(batch["text"], padding=True, truncation=True, max_length=128)
+   tokenized_batch["label"] = [label2score[label] for label in batch["label"]]
+   return tokenized_batch
 
 train_dataset = dataset["train"]
 test_dataset = dataset["test"]
@@ -36,8 +32,8 @@ tokenized_test = train_dataset.map(preprocess_function, batched=True)
 ### TRAINING ###
 
 def compute_metrics(eval_pred):
-   load_accuracy = load_metric("accuracy")
-   load_f1 = load_metric("f1")
+   load_accuracy = load("accuracy")
+   load_f1 = load("f1")
   
    logits, labels = eval_pred
    predictions = np.argmax(logits, axis=-1)
@@ -48,21 +44,16 @@ def compute_metrics(eval_pred):
 model = AutoModelForSequenceClassification.from_pretrained("distilbert-base-uncased", num_labels=2)
 training_args = TrainingArguments(
    output_dir=OUT_DIR,
+   eval_steps=20,
+   evaluation_strategy="steps",
    report_to="wandb",
    learning_rate=2e-5,
    logging_steps=5,
-   per_device_train_batch_size=32,
-   per_device_eval_batch_size=32,
-   evaluation_strategy="steps",
-   eval_steps=20,
-   max_steps=100,
-   save_steps=100,
    per_device_train_batch_size=16,
    per_device_eval_batch_size=16,
    num_train_epochs=2,
    weight_decay=0.01,
    save_strategy="epoch",
-   push_to_hub=True,
 )
 trainer = Trainer(
    model=model,
@@ -70,7 +61,7 @@ trainer = Trainer(
    train_dataset=tokenized_train,
    eval_dataset=tokenized_test,
    tokenizer=tokenizer,
-   data_collator=DataCollatorWithPadding(tokenizer=tokenizer),
+   data_collator=DataCollatorWithPadding(tokenizer=tokenizer, padding="longest"),
    compute_metrics=compute_metrics,
 )
 
