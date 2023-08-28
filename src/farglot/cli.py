ad6cff80541d04f4b05be164c9d8be6a9f6d758a
michaelhly <michaelhly@gmail.com>
2023-08-27 21:31:55 -0400

    Add logging

--- src/farglot/cli.py
+++ src/farglot/cli.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from plistlib import InvalidFileException
 from typing import List, Optional, Tuple
 import csv
 import json
@@ -7,6 +8,10 @@ import rocksdb
 
 from farglot.__farcaster.generated.message_pb2 import Message
 
+FARGLOT_DIR = Path.home().joinpath(".farglot")
+FARGLOT_CONF_PATH = FARGLOT_DIR.joinpath("conf")
+DEFAULT_HUB_DB_DIR = Path.home().joinpath("apps/hubble/.rocks/rocks.hub._default")
+
 
 class Label:
     def __init__(self, name: str, default_value: Optional[str] = None):
@@ -14,23 +19,15 @@ class Label:
         self.default_value = default_value
 
 
-class Configs:
-    def __init__(self):
-        self.farglot_dir = Path.home().joinpath(".farglot")
-        self.default_hub_db_path = Path.home().joinpath("apps/hubble/.rocks/rocks.hub._default")
-
-
 app = typer.Typer()
-configs = Configs()
 
 
 @app.command()
 def init():
-    configs.farglot_dir.mkdir(exist_ok=True)
-    __write_paths_to_conf(
-        configs.farglot_dir.joinpath("labels.json"),
-        configs.default_hub_db_path
-    )
+    FARGLOT_DIR.mkdir(exist_ok=True)
+    __write_paths_to_conf(FARGLOT_DIR.joinpath("labels.json"), DEFAULT_HUB_DB_DIR)
+    typer.echo(f"FarGlot configs initialized at {str(FARGLOT_CONF_PATH)}")
+
 
 @app.command()
 def set_hub_db_path(db_path: str):
@@ -40,6 +37,8 @@ def set_hub_db_path(db_path: str):
 
     label_path, _ = __read_paths_from_conf()
     __write_paths_to_conf(label_path, hub_db_path)
+    typer.echo(f"Hub DB path updated to {db_path}")
+
 
 @app.command()
 def set_labels_path(label_path: str):
@@ -49,6 +48,7 @@ def set_labels_path(label_path: str):
 
     _, db_path = __read_paths_from_conf()
     __write_paths_to_conf(lp, db_path)
+    typer.echo(f"Labels path updated to {label_path}")
 
 
 @app.command()
@@ -57,8 +57,15 @@ def new_training_set(out: Optional[str] = None):
     if not labels_path.exists():
         raise FileNotFoundError(f"{str(labels_path)}")
 
+    typer.echo(f"Pulling labels from ${str(labels_path)} ...")
     with labels_path.open() as f:
-        labels_json = json.load(f)
+        try:
+            labels_json = json.load(f)
+        except:
+            raise InvalidFileException(
+                f"failed to decode labels from {str(labels_path)}"
+            )
+
         if type(labels_json) == list:
             labels_list: List[dict] = labels_json
             labels = [
@@ -69,29 +76,29 @@ def new_training_set(out: Optional[str] = None):
             labels_single_dict: dict = labels_json
             labels = [
                 Label(
-                    labels_single_dict["name"], labels_single_dict.get("default_value")
+                    labels_single_dict["name"],
+                    labels_single_dict.get("default_value"),
                 )
             ]
         else:
             raise ValueError("f{labels_json} is not valid")
 
-    print(labels)
     __generate_training_set_from_rocks_db(db_path, labels, out)
 
 
 def __generate_training_set_from_rocks_db(
-    hub_db_path: Path,
-    labels: List[Label],
-    out: Optional[str] = None
+    hub_db_path: Path, labels: List[Label], out: Optional[str] = None
 ):
     db = rocksdb.DB(
         str(hub_db_path),
         rocksdb.Options(create_if_missing=False),
     )
+
+    typer.echo(f"Pulling casts from ${str(hub_db_path)} ...")
     it = db.itervalues()
     it.seek_to_first()
 
-    out_path = Path(out) if out else configs.farglot_dir.joinpath("training-set.csv")
+    out_path = Path(out) if out else FARGLOT_DIR.joinpath("training-set.csv")
     with out_path.open(mode="w") as f:
         csv_writer = csv.writer(f)
 
@@ -111,20 +118,23 @@ def __generate_training_set_from_rocks_db(
                     row.extend(default_label_values)
                     csv_writer.writerow(row)
             except:
-                print("skpping non message type")
+                # typer.echo("skpping non message type")
+                continue
+
+    typer.echo(f"training-set.csv available at ${out_path.absolute()}")
+
 
 def __read_paths_from_conf() -> Tuple[Path, Path]:
-    conf_path = configs.farglot_dir.joinpath("conf")
-    with conf_path.open(mode="r") as conf:
+    with FARGLOT_CONF_PATH.open(mode="r") as conf:
         labels_path = conf.readline().strip("\n")
         db_path = conf.readline().strip("\n")
     return Path(labels_path), Path(db_path)
 
 
 def __write_paths_to_conf(labels_path: Path, db_path: Path):
-    conf_path = configs.farglot_dir.joinpath("conf")
-    with conf_path.open(mode="w") as conf:
+    with FARGLOT_CONF_PATH.open(mode="w") as conf:
         conf.writelines([str(labels_path) + "\n", str(db_path)])
+
 
 if __name__ == "__main__":
     app()
