d8c3db6700b743a9ce911858ac1613e80bc467db
michaelhly <michaelhly@gmail.com>
2023-08-27 21:40:41 -0400

    Rename label -> column

--- src/farglot/cli.py
+++ src/farglot/cli.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 from plistlib import InvalidFileException
-from typing import List, Optional, Tuple
+from typing import List, Optional, Tuple, Union
 import csv
 import json
 import typer
@@ -10,11 +10,14 @@ from farglot.__farcaster.generated.message_pb2 import Message
 
 FARGLOT_DIR = Path.home().joinpath(".farglot")
 FARGLOT_CONF_PATH = FARGLOT_DIR.joinpath("conf")
+DEFAULT_COLUMN_VALUE = 0
 DEFAULT_HUB_DB_DIR = Path.home().joinpath("apps/hubble/.rocks/rocks.hub._default")
 
 
-class Label:
-    def __init__(self, name: str, default_value: Optional[str] = None):
+class Column:
+    def __init__(
+        self, name: str, default_value: Optional[Union[int, str]] = DEFAULT_COLUMN_VALUE
+    ):
         self.name = name
         self.default_value = default_value
 
@@ -25,7 +28,7 @@ app = typer.Typer()
 @app.command()
 def init():
     FARGLOT_DIR.mkdir(exist_ok=True)
-    __write_paths_to_conf(FARGLOT_DIR.joinpath("labels.json"), DEFAULT_HUB_DB_DIR)
+    __write_paths_to_conf(FARGLOT_DIR.joinpath("columns.json"), DEFAULT_HUB_DB_DIR)
     typer.echo(f"FarGlot configs initialized at {str(FARGLOT_CONF_PATH)}")
 
 
@@ -35,59 +38,61 @@ def set_hub_db_path(db_path: str):
     if not hub_db_path.exists() or not hub_db_path.is_dir():
         raise ValueError(f"${db_path} is not a valid directory")
 
-    label_path, _ = __read_paths_from_conf()
-    __write_paths_to_conf(label_path, hub_db_path)
+    columns_path, _ = __read_paths_from_conf()
+    __write_paths_to_conf(columns_path, hub_db_path)
     typer.echo(f"Hub DB path updated to {db_path}")
 
 
 @app.command()
-def set_labels_path(label_path: str):
-    lp = Path(label_path)
+def set_columns_path(columns_path: str):
+    lp = Path(columns_path)
     if not lp.exists() or lp.is_dir():
-        raise ValueError(f"${label_path} is not a valid file")
+        raise ValueError(f"${columns_path} is not a valid file")
 
     _, db_path = __read_paths_from_conf()
     __write_paths_to_conf(lp, db_path)
-    typer.echo(f"Labels path updated to {label_path}")
+    typer.echo(f"Columns path updated to {columns_path}")
 
 
 @app.command()
 def new_training_set(out: Optional[str] = None):
-    labels_path, db_path = __read_paths_from_conf()
-    if not labels_path.exists():
-        raise FileNotFoundError(f"{str(labels_path)}")
+    columns_path, db_path = __read_paths_from_conf()
+    if not columns_path.exists():
+        raise FileNotFoundError(f"{str(columns_path)}")
 
-    typer.echo(f"Pulling labels from ${str(labels_path)} ...")
-    with labels_path.open() as f:
+    typer.echo(f"Pulling columns from ${str(columns_path)} ...")
+    with columns_path.open() as f:
         try:
-            labels_json = json.load(f)
+            columns_json = json.load(f)
         except:
             raise InvalidFileException(
-                f"failed to decode labels from {str(labels_path)}"
+                f"failed to decode columns from {str(columns_path)}"
             )
 
-        if type(labels_json) == list:
-            labels_list: List[dict] = labels_json
-            labels = [
-                Label(label["name"], label.get("default_value"))
-                for label in labels_list
+        if type(columns_json) == list:
+            columns_list: List[dict] = columns_json
+            columns = [
+                Column(
+                    column["name"], column.get("default_value", DEFAULT_COLUMN_VALUE)
+                )
+                for column in columns_list
             ]
-        elif type(labels_json) == dict:
-            labels_single_dict: dict = labels_json
-            labels = [
-                Label(
-                    labels_single_dict["name"],
-                    labels_single_dict.get("default_value"),
+        elif type(columns_json) == dict:
+            column_single_dict: dict = columns_json
+            columns = [
+                Column(
+                    column_single_dict["name"],
+                    column_single_dict.get("default_value", DEFAULT_COLUMN_VALUE),
                 )
             ]
         else:
-            raise ValueError("f{labels_json} is not valid")
+            raise ValueError("f{columns_json} is not valid")
 
-    __generate_training_set_from_rocks_db(db_path, labels, out)
+    __generate_training_set_from_rocks_db(db_path, columns, out)
 
 
 def __generate_training_set_from_rocks_db(
-    hub_db_path: Path, labels: List[Label], out: Optional[str] = None
+    hub_db_path: Path, columns: List[Column], out: Optional[str] = None
 ):
     db = rocksdb.DB(
         str(hub_db_path),
@@ -102,11 +107,11 @@ def __generate_training_set_from_rocks_db(
     with out_path.open(mode="w") as f:
         csv_writer = csv.writer(f)
 
-        label_names = ["hash", "text"]
-        label_names.extend([label.name for label in labels])
-        csv_writer.writerow(label_names)
+        column_names = ["hash", "text"]
+        column_names.extend([column.name for column in columns])
+        csv_writer.writerow(column_names)
 
-        default_label_values = [label.default_value for label in labels]
+        default_column_values = [column.default_value for column in columns]
         for serialized in it:
             try:
                 m = Message()
@@ -115,7 +120,7 @@ def __generate_training_set_from_rocks_db(
                     hash = f"0x{m.hash.hex()}"
                     text = m.data.cast_add_body.text
                     row = [hash, text]
-                    row.extend(default_label_values)
+                    row.extend(default_column_values)
                     csv_writer.writerow(row)
             except:
                 # typer.echo("skpping non message type")
@@ -126,14 +131,14 @@ def __generate_training_set_from_rocks_db(
 
 def __read_paths_from_conf() -> Tuple[Path, Path]:
     with FARGLOT_CONF_PATH.open(mode="r") as conf:
-        labels_path = conf.readline().strip("\n")
+        columns_path = conf.readline().strip("\n")
         db_path = conf.readline().strip("\n")
-    return Path(labels_path), Path(db_path)
+    return Path(columns_path), Path(db_path)
 
 
-def __write_paths_to_conf(labels_path: Path, db_path: Path):
+def __write_paths_to_conf(columns_path: Path, db_path: Path):
     with FARGLOT_CONF_PATH.open(mode="w") as conf:
-        conf.writelines([str(labels_path) + "\n", str(db_path)])
+        conf.writelines([str(columns_path) + "\n", str(db_path)])
 
 
 if __name__ == "__main__":
